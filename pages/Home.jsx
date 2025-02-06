4bf129bd9cdf4bb33d56c8bfd6c7fdd7e1317734
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-02-06 12:10:59 +0200

    Initialize miss books project

---
+++ pages/Home.jsx
@@ -0,0 +1,10 @@
+// import { UserPreview } from '../cmps/UserPreview.jsx'
+
+export function Home() {
+    return (
+        <section>
+            <h2></h2>
+            {/* <UserPreview /> */}
+        </section>
+    )
+}
