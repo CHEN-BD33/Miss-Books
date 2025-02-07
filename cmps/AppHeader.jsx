f1ca088e98ae21e74c2360fd9fda5d4573b1c8a4
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-02-07 09:14:06 +0200

    Change the nav order

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -7,8 +7,8 @@ export function AppHeader({ onSetPage }) {
                 <h1>Books Shop App</h1>
                 <nav className="app-nav">
                     <a onClick={() => onSetPage('home')} href="#">Home</a>
-                    <a onClick={() => onSetPage('about')} href="#">About</a>
                     <a onClick={() => onSetPage('book')} href="#">Books</a>
+                    <a onClick={() => onSetPage('about')} href="#">About</a>
                 </nav>
             </section>
         </header>
