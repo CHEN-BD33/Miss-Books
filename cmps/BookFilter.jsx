c17898577bc856698ce4a020abbee7e48609c96a
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-02-08 16:09:49 +0200

    Change the filter section from submit

--- cmps/BookFilter.jsx
+++ cmps/BookFilter.jsx
@@ -11,21 +11,21 @@ export function BookFilter({ filterBy, onSetFilterBy }) {
     function onHandleChange(ev) {
         let { value, type, name: field } = ev.target
 
-        if (type === 'number') value = +value
+        if (type === 'number') value = +value || ''
         setfilterByToEdit(prevFilterBy => ({ ...prevFilterBy, [field]: value }))
     }
 
-    function onSubmitForm(ev) {
-        ev.preventDefault()
+    // function onSubmitForm(ev) {
+    //     ev.preventDefault()
 
-        onSetFilterBy(filterByToEdit)
-    }
+    //     onSetFilterBy(filterByToEdit)
+    // }
 
     return (
         <section className="Book-filter">
             <h2>Filter Our Books</h2>
 
-            <form onSubmit={onSubmitForm}>
+            <div className="filter-container">
                 <label htmlFor="title">Title</label>
                 <input name="title" value={filterByToEdit.title} onChange={onHandleChange} type="text" id="title" />
 
@@ -39,15 +39,14 @@ export function BookFilter({ filterBy, onSetFilterBy }) {
                 <input name="toYear" value={filterByToEdit.toYear || ''} onChange={onHandleChange} type="number" id="toYear" />
 
                 <label htmlFor="language">Language:</label>
-            <select name="language" value={filterByToEdit.language || ''} onChange={onHandleChange} id="language" >
+            <select name="language" value={filterByToEdit.language} onChange={onHandleChange} id="language" >
                 <option value="">All</option>
                 <option value="en">English</option>
                 <option value="he">Hebrew</option>
                 <option value="sp">Spanish</option>
             </select>
 
-                {/* <button>Submit</button> */}
-            </form>
+            </div>
 
         </section>
     )
